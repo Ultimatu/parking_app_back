f13c48df627fb0d107553895370b445036e13f5f
Ultimatu <tondesoulco@gmail.com>
2023-11-11 16:35:09 +0000

    update

--- src/assignement/assignement.controller.ts
+++ src/assignement/assignement.controller.ts
@@ -10,6 +10,7 @@ import {
   UseGuards,
   ValidationPipe,
   UsePipes,
+  Logger,
 } from '@nestjs/common';
 import { AssignementService } from './assignement.service';
 import { CreateAssignmentDto } from './dto/create-assignement.dto';
@@ -135,14 +136,18 @@ export class AssignementController {
   @Get('my-parkings/:id')
   @ApiOperation({ summary: 'Get assignments for a specific car' })
   @ApiParam({ name: 'id', type: 'number' })
-  async getAssignmentsForCar(
+  async getAssignmentsUser(
     @Param('id') id: number,
     @Res() res,
   ): Promise<Assignment[]> {
     try {
+      Logger.log('getAssignmentsUser');
       const assignments = await this.assignementService.findUserAssignment(id);
+      Logger.log('assignments: ' + assignments);
+      Logger.log('assignments: ' + JSON.stringify(assignments.assignments));
       return res.status(200).json(assignments);
     } catch (err) {
+      Logger.log('error: ' + err);
       return res.status(400).json({ message: err.message });
     }
   }
